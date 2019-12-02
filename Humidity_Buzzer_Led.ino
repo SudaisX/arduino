388e53650b6010694e28dd869b659d40b41a92f2
Sudais <44402465+sudaisyasin@users.noreply.github.com>
2019-12-02 18:02:31 +0500

    Add files via upload

---
+++ Humidity_Buzzer_Led.ino
@@ -0,0 +1,93 @@
+#include <DHT.h> 
+#define DHTTYPE DHT11
+#define DHTPIN 8
+
+#define b 9
+#define c 13
+#define d 10
+#define e 11
+#define f 12
+#define g 6
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+    pinMode(b, OUTPUT);
+    pinMode(c, OUTPUT);
+    pinMode(d, OUTPUT);
+    pinMode(e, OUTPUT);
+    pinMode(f, OUTPUT);
+    pinMode(g, OUTPUT);
+    pinMode(7, OUTPUT);  //Led and Buzzer
+    
+    Serial.begin(9600);
+    Serial.println("Program Started!");
+    dht.begin();
+
+}
+
+void loop() {
+ delay(2000); // Wait a few seconds between measurements
+ float h = dht.readHumidity();  // Reading temperature or humidity takes about 250 milliseconds!
+ float t = dht.readTemperature(); // Read temperature as Celsius (the default)
+ float f_temp = dht.readTemperature(true); // Read temperature as Fahrenheit (isFahrenheit = true) 
+ 
+ if (isnan(h) || isnan(t) || isnan(f_temp))  // Check if any reads failed and exit early (to try again).
+{
+ Serial.println("Failed to read from DHT sensor!");
+ return;
+ }
+
+ if(h < 41){
+  
+  digitalWrite(b, LOW);
+  digitalWrite(c, LOW);
+  digitalWrite(d, HIGH); //
+  digitalWrite(e, HIGH); //
+  digitalWrite(f, HIGH); //
+  digitalWrite(g, LOW);
+  digitalWrite(7, LOW);
+  Serial.print ("Humidity: ");
+  Serial.print (h);
+  Serial.print (" %\t");
+  Serial.print ("Humidity < 40% hense 'L' \t");
+  Serial.print ("Relay = OFF\t");
+  
+  
+  
+ }else{
+  if(h > 40 && h < 71){
+    
+   digitalWrite(b, LOW);
+   digitalWrite(c, HIGH); //
+   digitalWrite(d, HIGH); //
+   digitalWrite(e, HIGH); //
+   digitalWrite(f, HIGH); //
+   digitalWrite(g, LOW);
+   digitalWrite(7, LOW);
+   Serial.print ("Humidity: ");
+   Serial.print (h);
+   Serial.print (" %\t");
+   Serial.print ("40% < Humidity > 70% hense 'U' \t");
+   Serial.print ("Relay = OFF");
+   
+  }else{
+    if(h < 70 && h < 101){
+      
+      digitalWrite(b, HIGH); //
+      digitalWrite(c, HIGH); //
+      digitalWrite(d, LOW);
+      digitalWrite(e, HIGH); //
+      digitalWrite(f, HIGH); //
+      digitalWrite(g, HIGH); //
+      digitalWrite(7, HIGH);
+      Serial.print ("Humidity: ");
+      Serial.print (h);
+      Serial.print (" % \t");
+      Serial.print ("70% < Humidity < 100% hense 'H' \t");
+      Serial.print ("Relay = ON\t");
+      
+    }
+  }
+ }
+ }
